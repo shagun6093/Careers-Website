0ca0d61f9a2ed315b1819ac67214a3e2dd5f2620
shagun6093 <102024729+shagun6093@users.noreply.github.com>
2023-07-01 14:39:25 +0000

    changes before deploying

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template ,jsonify
 app =Flask(__name__)
+
+JOBS =[
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Delhi ,India',
+    'salary' : 'RS 15,00,000'
+  },
+ {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Delhi ,India',
+    'salary' : 'RS 15,00,000'
+  },
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Delhi ,India',
+    'salary' : 'RS 15,00,000'
+  },
+
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Delhi ,India',
+    'salary' : 'RS 15,00,000'
+  },
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Delhi ,India',
+    
+  }
+
+]
 
 @app.route("/")
 def hello_world():
-  return "Hello Shagun"
+  return render_template('home.html' , jobs =JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ =="__main__":
   app.run(host= '0.0.0.0', debug = True)
